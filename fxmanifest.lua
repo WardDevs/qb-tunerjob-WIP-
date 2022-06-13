e3fcb6f324b431b0d01e8110586ad66c5d945479
Sma Dababa Xor <104497157+WardDevs@users.noreply.github.com>
2022-06-13 21:22:28 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'qb-template'
+description 'qb-tunerjob'
 version '1.0.0'
-author 'QBCore Framework'
+author 'Ward Developments'
 
 shared_scripts {
     'config/sh_config.lua'
