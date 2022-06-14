ed51c9b6a351abf907c55479ef0d2f0e5d3c459b
Sma Dababa Xor <104497157+WardDevs@users.noreply.github.com>
2022-06-15 01:23:58 +0300

    Added log

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,24 +1,27 @@
 local QBCore = exports['qb-core']:GetCoreObject() -- Grabs our core object
+local playerData = nil
 
 RegisterServerEvent("qb-tunerjob:bill:player")
 AddEventHandler("qb-tunerjob:bill:player", function(playerId, amount)
-        local biller = QBCore.Functions.GetPlayer(source)
+        local xPlayer = QBCore.Functions.GetPlayer(source)
+        local fname = Player.PlayerData.charinfo.firstname
+        local lname = Player.PlayerData.charinfo.lastname
         local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
         local amount = tonumber(amount)
-        if biller.PlayerData.job.name == 'tuner' then
+        if xPlayer.PlayerData.job.name == 'tuner' then
             if billed ~= nil then
-                if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
+                if xplayer.PlayerData.citizenid ~= billed.PlayerData.citizenid then
                     if amount and amount > 0 then
                         MySQL.Async.insert('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (:citizenid, :amount, :society, :sender)', {
                             ['citizenid'] = billed.PlayerData.citizenid,
                             ['amount'] = amount,
-                            ['society'] = biller.PlayerData.job.name,
-                            ['sender'] = biller.PlayerData.charinfo.firstname
+                            ['society'] = xPlayer.PlayerData.job.name,
+                            ['sender'] = xPlayer.PlayerData.charinfo.firstname
                         })
                         TriggerClientEvent('qb-phone:RefreshPhone', billed.PlayerData.source)
                         TriggerClientEvent('QBCore:Notify', source, 'Invoice Successfully Sent', 'success')
                         TriggerClientEvent('QBCore:Notify', billed.PlayerData.source, 'New Invoice Received')
-                        TriggerEvent("qb-log:server:CreateLog", "tuner", "Tuner Bill", "red", " | "..biller.." billed "..billed.." for"..amount.."$")
+                        TriggerEvent("qb-log:server:CreateLog", "tuner", "Tuner Bill", "red", " | "..fname.." "..lname.." billed "..billed.." for"..amount.."$") 
                     else
                         TriggerClientEvent('QBCore:Notify', source, 'Must Be A Valid Amount Above 0', 'error')
                     end
@@ -35,8 +38,9 @@ end)
 
 RegisterServerEvent("qb-tunerjob:insure:veh")
 AddEventHandler("qb-tunerjob:insure:veh", function(Plate, Model, Name)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local fname = Player.PlayerData.charinfo.firstname
+    local lname = Player.PlayerData.charinfo.lastname
     info = {
         plate = Plate,
         model = Model,
@@ -44,6 +48,10 @@ AddEventHandler("qb-tunerjob:insure:veh", function(Plate, Model, Name)
     }
     if Player.Functions.AddItem("tunerinsurance", 1, false, info) then
         TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items["tunerinsurance"], "add")
+        TriggerClientEvent('QBCore:Notify', source, 'Vehicle Insured', 'success')
+        TriggerEvent("qb-log:server:CreateLog", "tuner", "Vehicle Insured", "green", " | "..fname.." "..lname.." Insured Model: "..Model.." With plate "..Plate.. " Name: "..Name)
+        --[[ TriggerEvent("qb-log:server:CreateLog", "tuner", "Tuner Bill", "red", " | "..fname.." "..lname.." Created insurance" "Plate"..Plate.. "Model" ..Model.." Name" ..Name.. ) ]]
+        
     end
-    TriggerEvent("qb-log:server:CreateLog", "tuner", "Tuner Insurance", "red", " | "..player.." created an insurance " "plate" ..plate.." model" ..amount.. "name"..name)
+    
 end)
